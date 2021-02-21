a9fc0eb596c2075f23e3ae959f3d9184cd921f06
Atli Sigurgeirsson <s2063518@escience6.inf.ed.ac.uk>
2021-02-21 19:13:49 +0000

    fixed an issue with wrong type in utils.get_mask_from_lengths

--- fastspeech2/utils.py
+++ fastspeech2/utils.py
@@ -110,7 +110,7 @@ def get_mask_from_lengths(lengths, max_len=None):
     lengths = lengths.float()
     batch_size = lengths.shape[0]
     if max_len is None:
-        max_len = torch.max(lengths).item()
+        max_len = int(torch.max(lengths).item())
 
     ids = torch.arange(0, max_len).unsqueeze(
         0).expand(batch_size, -1).to(device)
