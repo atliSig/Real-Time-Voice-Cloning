8dab181dffc37cbd7907afa45f7f0d3c9e77a323
Atli Sigurgeirsson <s2063518@escience6.inf.ed.ac.uk>
2021-02-21 19:14:47 +0000

    gen_embeddings and cosine similarity working

---
+++ speaker_verification/gen_embeddings.py
@@ -0,0 +1,53 @@
+import sys
+import os
+from pathlib import Path
+
+from voice_cloning.encoder.params_model import model_embedding_size as speaker_embedding_size
+from voice_cloning.utils.argutils import print_args
+from voice_cloning.encoder import inference as encoder
+
+import numpy as np
+import soundfile as sf
+import librosa
+import argparse
+import torch
+from audioread.exceptions import NoBackendError
+
+if __name__ == '__main__':
+    ## Info & args
+    parser = argparse.ArgumentParser(
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter
+    )
+    parser.add_argument("-e", "--enc_model_fpath", type=Path,
+        default="./voice_cloning/encoder/saved_models/pretrained.pt",
+        help="Path to a saved encoder")
+    parser.add_argument("--audio_dir",
+        default='./speaker_verification/ground_truth_audio',
+        help="A path to a directory of ground truth audio")
+    parser.add_argument("--output_dir",
+        default='./speaker_verification/embeddings',
+        help="A path to a directory where generated embeddings will be stored")
+    args = parser.parse_args()
+    print_args(args, parser)
+
+    encoder.load_model(args.enc_model_fpath)
+
+    if not os.path.exists(args.output_dir):
+        os.mkdir(args.output_dir)
+    for subdir, dirs, files in os.walk(args.audio_dir):
+        for audio_file in files:
+            audio_fid, ext = os.path.splitext(audio_file)
+            if ext in ['.wav', '.mp3']:
+                in_fpath = Path(os.path.join(subdir, audio_file))
+                out_fpath = Path(os.path.join(
+                    args.output_dir,
+                    *in_fpath.parent.parts[-2:],
+                    f'{audio_fid}.npy'))
+                Path(out_fpath.parent).mkdir(parents=True, exist_ok=True)
+
+                preprocessed_wav = encoder.preprocess_wav(in_fpath)
+                original_wav, sampling_rate = librosa.load(str(in_fpath))
+                preprocessed_wav = encoder.preprocess_wav(original_wav, sampling_rate)
+
+                embed = encoder.embed_utterance(preprocessed_wav)
+                np.save(out_fpath, embed)
---
+++ speaker_verification/measure_similarity.py
@@ -0,0 +1,44 @@
+import os
+import argparse
+import numpy as np
+
+def cos_sim(x, y):
+    return np.dot(x, y)/(np.linalg.norm(x) * np.linalg.norm(y))
+
+if __name__ == '__main__':
+    ## Info & args
+    parser = argparse.ArgumentParser(
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter
+    )
+    parser.add_argument("--embed_dir",
+        default='./speaker_verification/embeddings',
+        help="A path to a directory where generated embeddings will be stored")
+    parser.add_argument("--out_path",
+        default='./speaker_verification/results.tsv',
+        help="A path to a TSV file where generated results will be written")
+    args = parser.parse_args()
+
+    gt_dir = os.path.join(args.embed_dir, 'ground_truth_audio')
+    synth_dir = os.path.join(args.embed_dir, 'synthesized_audio')
+
+    assert os.path.isdir(gt_dir), 'Ground truth audio directory not found'
+    assert os.path.isdir(synth_dir), 'Synthesized audio directory not found'
+
+    out_file = open(args.out_path, 'w')
+
+    for gt_subdir, gt_dirs, gt_files in os.walk(gt_dir):
+        for gt_audio in gt_files:
+            ground_truth = np.load(os.path.join(gt_subdir, gt_audio))
+            gt_id = os.path.splitext(gt_audio)[0]
+            gt_speaker = gt_subdir
+
+            for s_subdir, s_dirs, s_files in os.walk(synth_dir):
+                for s_audio in s_files:
+                    synthesized = np.load(os.path.join(s_subdir, s_audio))
+                    s_id = os.path.splitext(s_audio)[0]
+                    s_speaker = s_subdir
+
+                    similarity = cos_sim(ground_truth, synthesized)
+
+                    out_file.write(f'{gt_id}\t{gt_speaker}\t{s_id}\t{s_speaker}\t{similarity}\n')
+
