7712e514a98731a20039bf4aa3cd2f9b1f48226d
Rokas Giedraitis <rokasgied@gmail.com>
2021-03-03 13:22:14 +0000

    Added local training file

---
+++ scripts/local_experiment_scripts/start_training.sh
@@ -0,0 +1,4 @@
+export DATASET_DIR=~/data/datasets/VCTK
+export MODEL_DIR=/home/s1661552/data/models/
+export PYTHONPATH=/home/s1661552/Real-Time-Voice-Cloning
+python fastspeech2/train.py --experiment-name ""
